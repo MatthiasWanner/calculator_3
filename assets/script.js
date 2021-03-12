c2eeaa1565b9ad7f7b310e2438e43929b40fd403
MatthiasWanner <matthias.wanner@gmail.com>
2021-03-12 12:02:38 +0100

    Decimals management with function checkDecimal and add comments

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
 const calculatorScreen = document.getElementById('calcul-screen').querySelector('p');
 const messageScreen = document.getElementById('message-screen').querySelector('p');
-const authorizedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "="];
-const operators = ["+", "-", "*", "/"];
+const calculatorTitle = messageScreen.textContent;
+const authorizedKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "*", "/", "=", ".", ","];
+const operators = ["+", "-", "X", "/", "*"];
 let textInScreen = "";
 
 // To insert content at the end of string to the calculator screen
@@ -13,9 +14,18 @@ const addToScreen = (string) => {
   let isOperatorKey = operators.includes(string);
   if(isOperatorKey === true){
     string = ` ${string} `;
-  }
-  pushInScreen = calculatorScreen.textContent + string;
-  calculatorScreen.innerHTML = pushInScreen;
+  }else if(string === "," || string === "."){
+    string = ".";
+    if(checkDecimal(calculatorScreen.textContent) === true){
+      errorMessage("Il y a déjà assez de décimale", calculatorTitle);
+    }else{
+      pushInScreen = calculatorScreen.textContent + string;
+      calculatorScreen.innerHTML = pushInScreen;
+    }
+  }else{
+    pushInScreen = calculatorScreen.textContent + string;
+    calculatorScreen.innerHTML = pushInScreen;
+  } 
 }
 
 // To remove last caracter writen in the claculator screen
@@ -57,13 +67,28 @@ const replaceOperator = (string, operator, array) => {
   calculatorScreen.innerHTML = newString;
 };
 
-const doTheCalcul = (string) => {
+// Check if décimal is already written in current number
+const checkDecimal = (string) => {
   strArray = string.split(" ");
-  let firstValue = parseInt(strArray[0]);
+  strArray.forEach(function(item){
+    let splittedWord = [];
+    splittedWord = item.split('');
+    splittedWord.forEach(function(){
+      isAlreadyDecimal = splittedWord.includes(".");
+    })
+  })
+  return isAlreadyDecimal; //true or false
+}
+
+
+const doTheCalcul = (string) => {
+  //convert string in array like ["number1", "operator", "number2"];
+  strArray = string.split(" ");
+  let firstValue = parseFloat(strArray[0]);
   let operator = strArray[1];
-  let secondValue = parseInt(strArray[2]);
+  let secondValue = parseFloat(strArray[2]);
   if( (operator === "/") && (secondValue === 0)){
-    result = "Invalid operator";
+    errorMessage(`Division par 0 impossible`, calculatorTitle);
   }else{
     switch (operator) {
         case '+':
@@ -72,34 +97,37 @@ const doTheCalcul = (string) => {
         case '-':
             result = firstValue - secondValue;
             break;
-        case '*':
+        case 'X':
             result = firstValue * secondValue;
             break;
         case '/':
             result = firstValue / secondValue;
             break;
         default:
-            result = " ";
+            result = firstValue;
         }
-  return result;
   }
+  return result;
 }
 
 const clicListener = (e) => {
   //console.log(e);
-  if(e.target.textContent === "C"){
+  let buttonClicked = e.target.textContent;
+  if(buttonClicked === "C"){
     removeFromScreen();
-  }else if(e.target.textContent === "Calculate"){
+  }else if(buttonClicked === "R"){
+    calculatorScreen.innerHTML = "";
+  }else if(buttonClicked === "Calculate"){
     let operation = calculatorScreen.textContent;
     let calcul = doTheCalcul(operation);
     calculatorScreen.innerHTML = calcul;
   }else{
-    isOperatorBtn = operators.includes(e.target.textContent);
+    isOperatorBtn = operators.includes(buttonClicked);
     if(isOperatorBtn === true){
       textInScreen = calculatorScreen.textContent;
-      replaceOperator(textInScreen, e.target.textContent, operators);
+      replaceOperator(textInScreen, buttonClicked, operators);
     }else{
-      addToScreen(e.target.textContent);
+      addToScreen(buttonClicked);
     }
   }
   e.preventDefault();
@@ -124,55 +152,21 @@ const keyboardListener = (e) => {
     let calcul = doTheCalcul(operation);
     calculatorScreen.innerHTML = calcul;
   }else{
-    messageScreen.innerHTML = `C'est une calculette pas un dico :)`;
+    errorMessage(`C'est une calculette pas un dico :)`);
   }
   e.preventDefault();
 }
 
-// Function which manage listeners
-// const listener = (e) => {
-//   if(e.type === "click"){
-//     //console.log(e);
-//     if(e.target.textContent === "C"){
-//       removeFromScreen();
-//     }else if(e.target.textContent === "Calculate"){
-//       let operation = calculatorScreen.textContent;
-//       let calcul = doTheCalcul(operation);
-//       calculatorScreen.innerHTML = calcul;
-//     }else{
-//       isOperatorBtn = operators.includes(e.target.textContent);
-//       if(isOperatorBtn === true){
-//         textInScreen = calculatorScreen.textContent;
-//         replaceOperator(textInScreen, e.target.textContent, operators);
-//       }else{
-//         addToScreen(e.target.textContent);
-//       }
-//     }
-//   }
-//   if(e.type === "keypress"){
-//     //console.log(e.key);
-//     keyPressed = e.key;
-//     let isAuthorizedKeys = authorizedKeys.includes(keyPressed);
-//     let isOperatorKey = operators.includes(keyPressed);
-//     if(keyPressed === "c"){
-//     removeFromScreen();
-//     }else if(isAuthorizedKeys === true && isOperatorKey === false){
-//     addToScreen(keyPressed);
-//     }else if(isOperatorKey === true){
-//       textInScreen = calculatorScreen.textContent;
-//       replaceOperator(textInScreen, keyPressed, operators);
-//     }else if(keyPressed === "r"){
-//       calculatorScreen.innerHTML = "";
-//     }else if(keyPressed === "Enter"){
-//       let operation = calculatorScreen.textContent;
-//       let calcul = doTheCalcul(operation);
-//       calculatorScreen.innerHTML = calcul;
-//     }else{
-//       messageScreen.innerHTML = `C'est une calculette pas un dico :)`;
-//     }
-//     e.preventDefault();
-//   }
-// }
+const errorMessage = (string, initialString) => {
+  document.getElementById('calcul-screen').classList.add("error");
+  messageScreen.innerHTML = string;
+  setTimeout(function(){
+    document.getElementById('calcul-screen').classList.remove("error")
+  }, 3000);
+  setTimeout(function(){
+    messageScreen.innerHTML = initialString;
+  }, 3000);
+}
 
 // Listenning clics on numbers buttons and print his value in the screen
 allButtons.forEach(function(item){
